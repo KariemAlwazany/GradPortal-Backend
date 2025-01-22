adce1c7af35c505838c10ed7a52e34966ca6ce61
YazanM23 <yazan.mansour2003@gmail.com>
2025-01-22 18:52:29 +0200

    final push

--- routes/notificationRouter.js
+++ routes/notificationRouter.js
@@ -7,6 +7,20 @@ const authController = require('./../controllers/authController');
 router.use(authController.protect);
 
 router.post('/notifyUser', Notification.notifyUserById);
+router.post('/notifyUserByUsername', Notification.notifyUserByUsername);
+
+router.post('/student/notifyDoctor', Notification.notifyDoctor);
+
+router.post(
+  '/student/request/notifyDoctor',
+  Notification.notifyDoctorforNewRequest,
+);
+router.post('/notifyDoctors', Notification.notifyAllDoctors);
+
+router.post('/doctor/notifyStudents', Notification.notifyAllStudents);
+router.post('/doctor/notifyMyStudents', Notification.notifyMyStudents);
+router.post('/doctor/notifyGroup/:groupID', Notification.notifyGroup);
+router.post('/adminsAndHeads', Notification.notifyAdminsAndHeads);
 router.post('/:notificationId/markAsRead', Notification.markNotificationAsRead);
 
 router.get('/:userId', Notification.getNotificationsByUser);
